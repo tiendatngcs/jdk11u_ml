cf1b146c1764d4a77503e0da8207f3d07194bb82
tiendatngcs <tiendat.ng.cs@gmail.com>
2021-11-09 18:28:37 -0600

    update shenandoahcontrolthread

--- src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
@@ -445,7 +445,7 @@ void ShenandoahControlThread::service_concurrent_normal_cycle(GCCause::Cause cau
                 "histogram: %s\n", heap->gc_epoch(), heap->capacity(), heap->used(), heap->committed(), heap->bytes_allocated_since_gc_start(), heap->hot_size(), heap->cold_size(), heap->num_regions(), heap->hot_region_count(), heap->cold_region_count(), "Placeholder");
   int arr_size = sizeof(heap->histogram()) / sizeof(heap->histogram()[0]);
   for (int i = 0; i < arr_size; i++){
-    log_info(gc)("\t%d: %d", i, heap->histogram()[i]);
+    log_info(gc)("\t%d: %lu", i, heap->histogram()[i]);
   }
   heap->refresh_hard_hot_cold_stats();
   heap->reset_histogram();

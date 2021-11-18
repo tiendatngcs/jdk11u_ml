815ada6e10457f5e759da256cce1141dd7e07c2f
Dat Nguyen <tiendat.ng.cs@gmail.com>
2021-11-17 21:53:39 -0600

    Add Qtable heuristics

--- src/hotspot/share/gc/shenandoah/mode/shenandoahIUMode.cpp
+++ src/hotspot/share/gc/shenandoah/mode/shenandoahIUMode.cpp
@@ -26,6 +26,7 @@
 #include "gc/shenandoah/heuristics/shenandoahAdaptiveHeuristics.hpp"
 #include "gc/shenandoah/heuristics/shenandoahAggressiveHeuristics.hpp"
 #include "gc/shenandoah/heuristics/shenandoahCompactHeuristics.hpp"
+#include "gc/shenandoah/heuristics/shenandoahQTableHeuristics.hpp"
 #include "gc/shenandoah/heuristics/shenandoahStaticHeuristics.hpp"
 #include "gc/shenandoah/mode/shenandoahIUMode.hpp"
 #include "logging/log.hpp"
@@ -65,6 +66,8 @@ ShenandoahHeuristics* ShenandoahIUMode::initialize_heuristics() const {
       return new ShenandoahAdaptiveHeuristics();
     } else if (strcmp(ShenandoahGCHeuristics, "compact") == 0) {
       return new ShenandoahCompactHeuristics();
+    } else if (strcmp(ShenandoahGCHeuristics, "qtable") == 0) {
+      return new ShenandoahQTableHeuristics();
     } else {
       vm_exit_during_initialization("Unknown -XX:ShenandoahGCHeuristics option");
     }
